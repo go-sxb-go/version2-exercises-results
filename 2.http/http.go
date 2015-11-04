7237afc5cb8bac618fee46bb3c0792252b3fdd3e
Soulou <leo@unbekandt.eu>
2015-11-04 21:10:23 +0100

    result of 2nd exercise

--- 2.http/http.go
+++ 2.http/http.go
@@ -1,7 +1,27 @@
 package http
 
+import (
+	"crypto/md5"
+	"fmt"
+	"io"
+	"net/http"
+)
+
 // GetGolang gets the front page of the golang homepage
 // and to compute its sum MD5, then return a human readable version
 // of the MD5
 func GetGolang() (string, error) {
+	res, err := http.Get("https://golang.org")
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+
+	hash := md5.New()
+	_, err = io.Copy(hash, res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%x", hash.Sum(nil)), nil
 }
