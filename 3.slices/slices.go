f29ea66c5154a8f01d991d4534672fdca03b8948
Soulou <leo@unbekandt.eu>
2015-11-04 21:54:33 +0100

    result of exercice 3 and 4

--- 3.slices/slices.go
+++ 3.slices/slices.go
@@ -5,8 +5,10 @@ package slices
 
 // Insert should add the value 'value' at the index 'index' of the slice 'slice'
 func Insert(slice []int, index, value int) []int {
+	return append(slice[:index], append([]int{value}, slice[index:]...)...)
 }
 
 // Delete should remove the index item from slice
 func Delete(slice []int, index int) []int {
+	return append(slice[:index], slice[index+1:]...)
 }
--- 4.compress/compress.go
+++ 4.compress/compress.go
@@ -1,19 +1,54 @@
 package compress
 
+import "compress/gzip"
+import "compress/lzw"
 import "io"
+import "io/ioutil"
 
 // CompressTo takes a file and a compression mode as input
 // And return the path to a temp file containing the compressed
 // version of the input
-// mode can be 'flate', 'gzip', or 'none' which will just copy the input in the file
+// mode can be 'lzw', 'gzip', or 'none' which will just copy the input in the file
 func CompressTo(input io.Reader, mode string) (string, error) {
+	file, err := ioutil.TempFile("", "go-compress")
+	if err != nil {
+		return "", err
+	}
+
+	switch mode {
+	case "lzw":
+		lzwCompress(input, file)
+	case "gzip":
+		gzipCompress(input, file)
+	case "none":
+		noneCompress(input, file)
+	}
+
+	file.Close()
+	return file.Name(), nil
 }
 
 func lzwCompress(input io.Reader, out io.Writer) error {
+	writer := lzw.NewWriter(out, lzw.MSB, 8)
+	_, err := io.Copy(writer, input)
+	if err != nil {
+		return err
+	}
+	writer.Close()
+	return nil
 }
 
 func gzipCompress(input io.Reader, out io.Writer) error {
+	writer := gzip.NewWriter(out)
+	_, err := io.Copy(writer, input)
+	if err != nil {
+		return err
+	}
+	writer.Close()
+	return nil
 }
 
 func noneCompress(input io.Reader, out io.Writer) error {
+	_, err := io.Copy(out, input)
+	return err
 }
